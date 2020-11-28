f955dd628851cb7b1b27e37dcbecfb6dcaab2665
charlesCXK <715811763@qq.com>
2020-11-28 14:17:22 +0800

    update

--- exp/cps.cifar10_label4000.dual_ws_lr0.01/run.sh
+++ exp/cps.cifar10_label4000.dual_ws_lr0.01/run.sh
@@ -10,14 +10,14 @@ export cxk_volna=$1"/"
 export snapshot_iter=1
 
 export learning_rate=0.03
-export batch_size=16 #16*4
+export batch_size=16 # 16*4
 export NGPUS=4
 export DATASET=cifar10
 export LABEL=4000
 
 python -m torch.distributed.launch --nproc_per_node $NGPUS ./train.py \
 --dataset $DATASET --num-labeled $LABEL --arch wideresnet --batch-size $batch_size --lr $learning_rate \
---expand-labels --seed 5 --out results/$DATASET@$LABEL --mu 10
+--expand-labels --seed 5 --out results/$DATASET@$LABEL --lr 0.01
 
 #python -m torch.distributed.launch --nproc_per_node 1 ./train.py \
 #--dataset cifar10 --num-labeled 4000 --arch wideresnet --batch-size 4 --lr 0.03 \
